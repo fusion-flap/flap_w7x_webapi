4419dc6b20c0a138abf19eb4d0b88991e2fd5d91
mvecsei <vecsei.miklos@energia.mta.hu>
2020-10-20 13:20:28 +0200

    added filterscope

--- webapi.py
+++ webapi.py
@@ -256,6 +256,12 @@ class GetSignal(object):
             stream_dict['ECE'+channel] = ['ArchiveDB/views/KKS/QME_ECE/standard_reduced/QME-ch'+channel]
 
 
+        #Filterscopte
+        for chnum in range(72):
+            label = 'TUBE'+"{:02d}".format(chnum+1)
+            chid = 'PMT0'+"{:02d}".format(chnum+1)
+            stream_dict['QSR02'+label] = 'Test/raw/W7X/Filterscopetest4/'+chid+'_DATASTREAM/V5/0/'+chid+'_Volts
+
         # This is EDICAM at the AEQ11 port:
         stream_dict['EDICAM-AEQ11'] = \
             'ArchiveDB/raw/W7X/QSV_VideoCameras_Edicam_AEQ11/ROIP1Data_DATASTREAM/0/image/unscaled'
